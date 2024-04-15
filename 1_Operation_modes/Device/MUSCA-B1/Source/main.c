df958741ae954eec52d7a749177089b3c033d205
Nguyen Thanh Trung 2 <trung.nguyen-thanh2@internal.banvien.com>
2024-04-15 16:05:57 +0700

    Add documents

--- 1_Operation_modes/Device/MUSCA-B1/Source/main.c
+++ 1_Operation_modes/Device/MUSCA-B1/Source/main.c
@@ -1,6 +1,34 @@
 #include "stdio.h"
+
+/* This function executes in THREAD MODE of the processor */
+void generate_interrupt()
+{
+    uint32_t *pSTIR  = (uint32_t*)0xE000EF00;
+    uint32_t *pISER0 = (uint32_t*)0xE000E100;
+
+    //enable IRQ3 interrupt
+    *pISER0 |= ( 1 << 3);
+
+    //generate an interrupt from software for IRQ3
+    *pSTIR = (3 & 0x1FF);
+
+}
+
+/* This function executes in THREAD MODE of the processor */
 int main(void)
 {
-    printf("Hello, Boot sequence is finished. I'm in main function now!!");
+    // Before interrupt
+
+    generate_interrupt();
+
+    // After interrupt
+
+    for(;;);
     return 0;
+}
+
+/* This function(ISR) executes in HANDLER MODE of the processor */
+void Interrupt3_Handler(void)
+{
+    // I'm in interrupt 3 ISA
 }
